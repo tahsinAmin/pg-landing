97c75241a1649af1379c9a4a44a08e3698ba68bc
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-19 15:04:39 +0600

    Building PG Landing Page

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ yarn create next-app --example with-tailwindcss with-tailwindcss-app
 ```
 
 Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
+
+# How to have your image resized to what you want.
+
+# How to give a bg-black overlay over a cover image.
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,199 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className='flex h-auto'>
+        <div className="sidebar fixed flex-none bg-blue-700 text-white h-screen p-2 flex flex-col justify-center">
+            <label htmlFor=""><span className="ti-menu"></span></label>
         </div>
-      </main>
+        <div className='flex-grow'>
+          <div className="flex flex-col">
+          <header className='main flex-none flex items-center justify-between my-2 w-11/12 mx-auto'>
+            <div className="brand flex items-center">
+              <div className="relative h-10 w-6 cursor-pointer my-auto"> 
+                  <Image
+                    src='/img/logo.jpg' 
+                    layout='fill' 
+                    objectFit='contain'
+                    objectPosition='left'/>
+              </div>
+              <p>NBA</p>
+            </div>
+            <div className="nav-menu">
+                <ul className='flex space-x-4 justify-between uppercase'>
+                  <li><a href="">Scores</a></li>
+                  <li><a href="">Schedule</a></li>
+                  <li><a href="">News</a></li>
+                  <li><a href="">Players</a></li>
+                  <li><a href="">Teams</a></li>
+                </ul>
+            </div>
+            <div className="user">
+              <div className="relative h-10 w-10 cursor-pointer my-auto"> 
+                <Image
+                src='/img/2.jpg' 
+                layout='fill' 
+                objectFit='cover'
+                className='rounded-full'
+                /> 
+              </div>
+            </div>
+          </header>
+          <div className="flex-grow bg-gray-200">
+            <div className="main-content w-11/12 mx-auto py-8">
+              <div className="content-1 grid grid-cols-3 gap-3 ">
+                <div className="side-content">
+                  <div className="fav flex items-center space-x-2">
+                    <div className="circle-bg flex justify-center items-center align-middle border-2 border-gray-200 bg-gray-300 rounded-full p-2">
+                      <span className="ti-star"></span>
+                    </div>
+                    <span>Favourites</span>
+                  </div>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+                  <div className="character">
+                    <h1 className='font-semibold text-5xl my-7 line leading-20'>Paul <br /> George</h1>
+                  </div>
+                  <div className="team flex items-center space-x-1">
+                    <div className="relative h-10 w-10 cursor-pointer my-auto"> 
+                      <Image
+                      src='/img/logo-sm.png' 
+                      layout='fill' 
+                      objectFit='cover'
+                      className='rounded-full'
+                      /> 
+                    </div>
+                    <span>LA Clippers</span>
+                  </div>
+                  <div className="features flex space-x-20 mt-7">
+                    <div className="feature">
+                      <p className='font-semibold'>HEIGHT</p>
+                      <h2 className='font-semibold text-xl'>6<small>ft</small>8<small>in</small></h2>
+                      <p>/203m</p>
+                    </div>
+                    <div className="feature">
+                      <p className='font-semibold'>WEIGHT</p>
+                      <h2 className='font-semibold text-xl'>220<small>lbs</small></h2>
+                      <p>/199.8Kg</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="middle">
+                  <div className="text-center flex justify-center items-center">
+                    <div className="relative h-36 w-full cursor-pointer my-auto"> 
+                      <Image
+                      src='/img/4.png' 
+                      layout='fill' 
+                      objectFit='cover'
+                      className=''
+                      /> 
+                    </div>
+                    
+                  </div>
+                <div className="relative">
+                  <div className="absolute z-50 scale-125 translate-x-1/4 -translate-y-24 h-[590px] w-[317px] cursor-pointer my-auto"> 
+                    <Image
+                      src='/img/paul.png' 
+                      layout='fill' 
+                      objectFit='cover'
+                      className='absolute'
+                    /> 
+                  </div>
+                  <h1 className='absolute -top-14 z-50 text-9xl  text-red-500 font-semibold left-6'>13</h1>
+                  <div className="absolute -top-10 translate-x-18 h-[400px] w-[400px] rounded-full border-gray-200 border-2 z-40"></div>
+                </div>
+                </div>
+                <div className="side-content">
+                    <div className="flex flex-row-reverse">
+                      <div>
+                        <h3 className="text-md font-semibold mb-2">LAST GAME</h3>
+                        <div className="game-card bg-white rounded-md p-3">
+                          <h3 className="title text-md font-semibold text-center">LEAGUE PASS</h3>
+                          <div className="game-teams flex space-x-8 items-center mt-4">
+
+                            <div className="game-team flex flex-col justify-center items-center">
+                              <h1 className='font-semibold text-2xl flex items-center space-x-3'>
+                                <div className="relative h-12 w-12 cursor-pointer my-auto"> 
+                                  <Image
+                                  src='/img/logo2-sm.png'
+                                  layout='fill' 
+                                  objectFit='contain'
+                                  className=''
+                                  /> 
+                                </div>
+                                <p>101</p>
+                              </h1>
+                              <b>4-8</b>
+                            </div>
+                            <div className="game-team flex flex-col justify-center items-center">
+                              <b>FINAL</b>
+                            </div>
+
+                            <div className="game-team flex flex-col justify-center items-center">
+                              <h1 className='font-semibold text-2xl flex justify-center items-center space-x-2'>
+                                <div className="relative h-10 w-10 cursor-pointer my-auto"> 
+                                  <Image
+                                  src='/img/logo-sm.png' 
+                                  layout='fill' 
+                                  objectFit='cover'
+                                  className=''
+                                  /> 
+                                </div>
+                                <p>120</p>
+                              </h1>
+                              <b>4-8</b>
+                            </div>
+                          </div>
+                          <div className="text-center date flex space-x-2 items-center justify-center font-semibold">
+                            <span className="ti-calendar"></span>
+                            <span className='text-sm'>05/02/2020</span>
+                          </div>
+
+                          <div className="highlights mt-4 grid grid-cols-2">
+                            <div className="h-card relative h-[150px] cursor-pointer " >
+                              <Image
+                                src='/img/1.jpg' 
+                                layout='fill' 
+                                objectFit='cover'
+                                className=''
+                              /> 
+                              <div className="h-card-over absolute h-full w-full rounded-md bg-black bg-opacity-50">
+                              </div>
+                            </div>
+                            <div className="h-card col-span-6 h-[150px] relative cursor-pointer " >
+                              <Image
+                                src='/img/1.jpg' 
+                                layout='fill' 
+                                objectFit='cover'
+                                className=''
+                              /> 
+                              <div className="h-card-over col-span-4 rounded-lg">
+                            </div>
+                          </div>
+                        </div>
+                          
+                      
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+              <div className="content-2">
+
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
